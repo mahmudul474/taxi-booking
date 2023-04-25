5553c009eba2b7586fba7a8bcb3376df6d9a097d
unknown <mahmudul474>
2023-04-25 16:54:48 +0600

    erpor home page desing thik korte gesi

--- src/Pages/Thaks.js
+++ src/Pages/Thaks.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const num=Math.random();
 const Thaks = () => {
   return (
+    <>
     <div className="w-full m-auto text-center  ">
       <h1 className="text-2xl">Youre order Succesfylly done </h1>
       <h2 className=" text-center">order id <span className="text-green-950 text-3xl ">{num}</span></h2>
+   
+   
+   <div className="flex justify-end mt-12">
+  <Link to="/">
+ <button className="btn bg-green-950">Back To Home </button></Link>
+</div>
+   
+   
     </div>
+
+
+
+    </>
   );
 };
 
--- src/components/RightSidebar/Jusrnydettails/JusnyDettaills.js
+++ src/components/RightSidebar/Jusrnydettails/JusnyDettaills.js
@@ -11,27 +11,31 @@ const JusnyDettaills = ({distance}) => {
    console.log(typeof(distance))
 
  
- 
+  const  singel= category && distance &&  category.price * distance
+  const  dubble= (category && distance &&  category.price * distance)*2
+
+
+ console.log(singel)
 
   return (
-    <div className="p-3 text-left capitalize ">
-      <h1 className="text-xl font-bold">From: {data  && data.origin }</h1>
-      <h1 className="text-xl font-bold">To: {data && data.destination }</h1>
+    <div className="p-3 text-left capitalize">
+      <h1 className=" text-lg font-bold">From: {data  && data.origin }</h1>
+      <h1 className=" text-lg font-bold">To: {data && data.destination }</h1>
 
-      <h1 className="text-xl font-bold">
+      <h1 className=" text-lg font-bold">
         DATE OF JOURNEY: {data&& data.startdate }
       </h1>
 
-   <h1 className="text-xl font-bold"> Returen Date: {data && data.returndate ? data.returndate  : "singel"}</h1> 
+   <h1 className=" text-lg font-bold"> Returen Date: {data && data.returndate ? data.returndate  : "singel"}</h1> 
 
 
  
 
     <div className="flex my-4 justify-start items-center">
     
-          <span className="  text-xl mr-7 flex justify-center"> Car-Type : {category && category.name} </span>
-          <span className="  text-xl mr-7 flex justify-center"><img className="w-7 mr-2 h-7" src="https://i.ibb.co/hBcRn5C/user.png"/> <p>{category && category.sit}</p> </span>
-          <span className="flex text-xl mr-7  justify-center"><img   className="w-7 mr-2 h-7 " src="https://i.ibb.co/rsJQX4K/luggage.png"/> <p>{category && category.luggage}</p></span>
+          <span className="   text-lg mr-7 flex justify-center"> Car-Type : {category && category.name} </span>
+          <span className="   text-lg mr-7 flex justify-center"><img className="w-7 mr-2 h-7" src="https://i.ibb.co/hBcRn5C/user.png"/> <p>{category && category.sit}</p> </span>
+          <span className="flex  text-lg mr-7  justify-center"><img   className="w-7 mr-2 h-7 " src="https://i.ibb.co/rsJQX4K/luggage.png"/> <p>{category && category.luggage}</p></span>
            
         </div>
     <div>
@@ -44,7 +48,9 @@ const JusnyDettaills = ({distance}) => {
 
    <div>
     <h1>Distance {distance} K.M</h1>
-    Total: { category && distance &&  category.price * distance} Taka
+    <hr/>
+    <h2 className="my-5 tex-xl capitalize  font-bold">Total:{data && data.returndate ==="" ? singel :dubble} Taka</h2>
+
    </div>
 
     </div>
