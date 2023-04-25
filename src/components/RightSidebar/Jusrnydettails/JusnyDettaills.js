03533bc2ba9eed02b676bb270fb297ce82879870
unknown <mahmudul474>
2023-04-25 16:03:54 +0600

    card hiseb ses

--- src/components/RightSidebar/Jusrnydettails/JusnyDettaills.js
+++ src/components/RightSidebar/Jusrnydettails/JusnyDettaills.js
@@ -6,9 +6,11 @@ const JusnyDettaills = ({distance}) => {
 
 
   
-  const {setData,data}=useContext(DataContext)
+  const {setData,data, category, setCategory, userInfo, setUserInfo}=useContext(DataContext)
 
-   
+   console.log(typeof(distance))
+
+ 
  
 
   return (
@@ -22,7 +24,29 @@ const JusnyDettaills = ({distance}) => {
 
    <h1 className="text-xl font-bold"> Returen Date: {data && data.returndate ? data.returndate  : "singel"}</h1> 
 
+
+ 
+
+    <div className="flex my-4 justify-start items-center">
     
+          <span className="  text-xl mr-7 flex justify-center"> Car-Type : {category && category.name} </span>
+          <span className="  text-xl mr-7 flex justify-center"><img className="w-7 mr-2 h-7" src="https://i.ibb.co/hBcRn5C/user.png"/> <p>{category && category.sit}</p> </span>
+          <span className="flex text-xl mr-7  justify-center"><img   className="w-7 mr-2 h-7 " src="https://i.ibb.co/rsJQX4K/luggage.png"/> <p>{category && category.luggage}</p></span>
+           
+        </div>
+    <div>
+      <h2>ADDITIONAL DETAILS</h2>
+      <p>phone: {userInfo&& userInfo.number}</p>
+    </div>
+
+   
+
+
+   <div>
+    <h1>Distance {distance} K.M</h1>
+    Total: { category && distance &&  category.price * distance} Taka
+   </div>
+
     </div>
   );
 };
